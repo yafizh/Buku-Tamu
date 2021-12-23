997ef00a6c776bc43eed211ad6c8ba8b6cd67283
yafizh <a.chairil.yafizh@gmail.com>
2021-12-23 22:15:21 +0800

    makes halaman_buku_tamu works

--- halaman_buku_tamu.php
+++ halaman_buku_tamu.php
@@ -1,3 +1,33 @@
+<?php
+if (isset($_POST['submit'])) {
+    require_once "koneksi.php";
+
+    $nama = $_POST['nama'];
+    $tanggal = $_POST['tanggal'];
+    $alamat = $_POST['alamat'];
+    $nomor_handphone = $_POST['nomor_handphone'];
+    $keperluan = $_POST['keperluan'];
+
+    $sql = "
+        INSERT INTO tabel_buku_tamu (
+            nama, 
+            tanggal, 
+            alamat, 
+            nomor_handphone, 
+            keperluan
+        ) VALUES (
+            '$nama', 
+            '$tanggal',
+            '$alamat', 
+            '$nomor_handphone',
+            '$keperluan'
+        )";
+
+    if ($mysqli->query($sql) === TRUE) echo "<script>alert('Tamu berhasil ditambahkan.')</script>";
+    else echo "Error: " . $sql . "<br>" . $mysqli->error;
+}
+
+?>
 <!-- MAIN -->
 <div class="main">
     <!-- MAIN CONTENT -->
@@ -13,37 +43,37 @@
                     <form action="" method="POST">
                         <div class="row" style="margin-bottom: 16px;">
                             <div class="col-md-12">
-                                <label for="exampleFormControlInput1" class="form-label">Nama</label>
-                                <input type="email" class="form-control" id="exampleFormControlInput1">
+                                <label for="nama" class="form-label">Nama</label>
+                                <input type="text" class="form-control" id="nama" name="nama">
                             </div>
                         </div>
                         <div class="row" style="margin-bottom: 16px;">
                             <div class="col-md-12">
-                                <label for="exampleFormControlInput1" class="form-label">Tanggal Bertamu</label>
-                                <input type="date" class="form-control" readonly id="exampleFormControlInput1" value="<?= Date("Y-m-d"); ?>">
+                                <label for="tanggal" class="form-label">Tanggal Bertamu</label>
+                                <input type="date" class="form-control" readonly id="tanggal" name="tanggal" value="<?= Date("Y-m-d"); ?>">
                             </div>
                         </div>
                         <div class="row" style="margin-bottom: 16px;">
                             <div class="col-md-12">
-                                <label for="exampleFormControlInput1" class="form-label">Alamat</label>
-                                <input type="text" class="form-control" id="exampleFormControlInput1">
+                                <label for="alamat" class="form-label">Alamat</label>
+                                <input type="text" class="form-control" id="alamat" name="alamat">
                             </div>
                         </div>
                         <div class="row" style="margin-bottom: 16px;">
                             <div class="col-md-12">
-                                <label for="exampleFormControlInput1" class="form-label">Nomor Handphone</label>
-                                <input type="number" class="form-control" id="exampleFormControlInput1">
+                                <label for="nomor_handphone" class="form-label">Nomor Handphone</label>
+                                <input type="number" class="form-control" id="nomor_handphone" name="nomor_handphone">
                             </div>
                         </div>
                         <div class="row" style="margin-bottom: 16px;">
                             <div class="col-md-12">
-                                <label for="exampleFormControlInput1" class="form-label">Keperluan</label>
-                                <textarea class="form-control" name="" id="" rows="3"></textarea>
+                                <label for="keperluan" class="form-label">Keperluan</label>
+                                <input type="text" class="form-control" id="keperluan" name="keperluan">
                             </div>
                         </div>
                         <div class="row" style="margin-bottom: 16px;">
                             <div class="col-md-12" style="display: flex; justify-content: end;">
-                                <button type="button" class="btn btn-primary">Tambah Tamu</button>
+                                <button type="submit" name="submit" class="btn btn-primary">Tambah Tamu</button>
                             </div>
                         </div>
                     </form>
