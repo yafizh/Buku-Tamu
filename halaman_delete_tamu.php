0f7f7967c8a5b06efab5735d619e8c06f1585cc9
yafizh <a.chairil.yafizh@gmail.com>
2021-12-23 22:43:50 +0800

    makes halaman_hapus_tamu.php works

---
+++ halaman_delete_tamu.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_GET['id_tamu'])) {
+    require_once "koneksi.php";
+
+    $sql = "DELETE FROM tabel_buku_tamu WHERE id_tamu=" . $_GET['id_tamu'];
+    if ($mysqli->query($sql) === TRUE) {
+        echo "<script>alert('Data Tamu Berhasil Dihapus.')</script>";
+        echo "<script>" .
+            "window.location.href='index.php?page=data_tamu';" .
+            "</script>";
+    } else echo "Error: " . $sql . "<br>" . $mysqli->error;    
+} else
+    echo "<script>" .
+        "window.location.href='index.php?page=data_tamu';" .
+        "</script>";
