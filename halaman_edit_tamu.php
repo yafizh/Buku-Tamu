c54d2df06249b42e95c26b1ad9eb1f5e8db0e511
yafizh <a.chairil.yafizh@gmail.com>
2021-12-23 22:42:23 +0800

    makes halaman_edit_tamu.php works

--- halaman_edit_tamu.php
+++ halaman_edit_tamu.php
@@ -1,3 +1,39 @@
+<?php
+
+if (isset($_GET['id_tamu'])) {
+    require_once "koneksi.php";
+
+    $sql = "SELECT * FROM tabel_buku_tamu WHERE id_tamu=" . $_GET['id_tamu'];
+    $result = $mysqli->query($sql);
+    $row = $result->fetch_assoc();
+} else
+    echo "<script>" .
+        "window.location.href='index.php?page=data_tamu';" .
+        "</script>";
+
+if (isset($_POST['submit'])) {
+    $nama = $_POST['nama'];
+    $alamat = $_POST['alamat'];
+    $nomor_handphone = $_POST['nomor_handphone'];
+    $keperluan = $_POST['keperluan'];
+
+    $sql = "UPDATE tabel_buku_tamu 
+            SET 
+                nama='$nama', 
+                alamat='$alamat', 
+                nomor_handphone='$nomor_handphone', 
+                keperluan='$keperluan'
+            WHERE 
+                id_tamu=" . $_GET['id_tamu'];
+
+    if ($mysqli->query($sql) === TRUE) {
+        echo "<script>alert('Tamu berhasil diedit.')</script>";
+        echo "<script>" .
+            "window.location.href='index.php?page=data_tamu';" .
+            "</script>";
+    } else echo "Error: " . $sql . "<br>" . $mysqli->error;
+}
+?>
 <!-- MAIN -->
 <div class="main">
     <!-- MAIN CONTENT -->
@@ -6,44 +42,44 @@
             <!-- OVERVIEW -->
             <div class="panel panel-headline">
                 <div class="panel-heading">
-                    <h3 class="panel-title">Edit Tamu Nurcholis</h3>
+                    <h3 class="panel-title">Edit Tamu <?= $row['nama']; ?></h3>
                     <!-- <p class="panel-subtitle">Periode: 1 Januar 14, 2016 - Oct 21, 2016</p> -->
                 </div>
                 <div class="panel-body">
                     <form action="" method="POST">
                         <div class="row" style="margin-bottom: 16px;">
                             <div class="col-md-12">
-                                <label for="exampleFormControlInput1" class="form-label">Nama</label>
-                                <input type="email" class="form-control" id="exampleFormControlInput1">
+                                <label for="nama" class="form-label">Nama</label>
+                                <input type="text" class="form-control" id="nama" name="nama" value="<?= $row['nama']; ?>">
                             </div>
                         </div>
                         <div class="row" style="margin-bottom: 16px;">
                             <div class="col-md-12">
-                                <label for="exampleFormControlInput1" class="form-label">Tanggal Bertamu</label>
-                                <input type="date" class="form-control" readonly id="exampleFormControlInput1" value="<?= Date("Y-m-d"); ?>">
+                                <label for="tanggal" class="form-label">Tanggal Bertamu</label>
+                                <input type="date" class="form-control" readonly id="tanggal" name="tanggal" value="<?= $row['tanggal']; ?>">
                             </div>
                         </div>
                         <div class="row" style="margin-bottom: 16px;">
                             <div class="col-md-12">
-                                <label for="exampleFormControlInput1" class="form-label">Alamat</label>
-                                <input type="text" class="form-control" id="exampleFormControlInput1">
+                                <label for="alamat" class="form-label">Alamat</label>
+                                <input type="text" class="form-control" id="alamat" name="alamat" value="<?= $row['alamat']; ?>">
                             </div>
                         </div>
                         <div class="row" style="margin-bottom: 16px;">
                             <div class="col-md-12">
-                                <label for="exampleFormControlInput1" class="form-label">Nomor Handphone</label>
-                                <input type="number" class="form-control" id="exampleFormControlInput1">
+                                <label for="nomor_handphone" class="form-label">Nomor Handphone</label>
+                                <input type="number" class="form-control" id="nomor_handphone" name="nomor_handphone" value="<?= $row['nomor_handphone']; ?>">
                             </div>
                         </div>
                         <div class="row" style="margin-bottom: 16px;">
                             <div class="col-md-12">
-                                <label for="exampleFormControlInput1" class="form-label">Keperluan</label>
-                                <textarea class="form-control" name="" id="" rows="3"></textarea>
+                                <label for="keperluan" class="form-label">Keperluan</label>
+                                <input type="text" class="form-control" id="keperluan" name="keperluan" value="<?= $row['keperluan']; ?>">
                             </div>
                         </div>
                         <div class="row" style="margin-bottom: 16px;">
                             <div class="col-md-12" style="display: flex; justify-content: end;">
-                                <button type="button" class="btn btn-warning">Edit Tamu</button>
+                                <button type="submit" name="submit" class="btn btn-primary">Edit Tamu</button>
                             </div>
                         </div>
                     </form>
