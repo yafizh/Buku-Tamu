9bcd986547c19afd3d75110a053575c2a2aceea3
yafizh <a.chairil.yafizh@gmail.com>
2021-12-24 13:50:39 +0800

    make pagination in halaman_data_tamu.php

--- database.sql
+++ database.sql
@@ -10,3 +10,17 @@ CREATE TABLE `tabel_buku_tamu` (
     keperluan VARCHAR(255) NOT NULL,
     PRIMARY KEY (id_tamu)
 );
+
+INSERT INTO `tabel_buku_tamu` (
+    nama,
+    alamat,
+    tanggal,
+    nomor_handphone,
+    keperluan
+) VALUES 
+('Nurcholis', 'Astambul', '2021-12-1', '111111111', 'Magang'), 
+('Nursahid Arya Suyudi', 'Binuang', '2021-11-1', '222222222', 'Berkunjung ke perpustakaan'), 
+('Diki Suti Prasetya', 'Banjarbaru', '2020-05-05', '333333333', 'Berkunjung'), 
+('Andry', 'Binuang', '2021-12-04', '444444444', 'Bertamu'), 
+('Rania Nor Aida', 'Astambul', '2021-10-01', '555555555', 'Baca Buku'), 
+('Ahmad Rifai', 'Martapura', '2021-12-03', '666666', 'Magang');
