cd53f715f2cd6402189f0c821ca7fef54c1302d7
yafizh <a.chairil.yafizh@gmail.com>
2022-01-27 16:10:49 +0800

    report based year

---
+++ halaman_laporan/cetak/laporan_tamu_per_tahun.php
@@ -0,0 +1,95 @@
+<?php
+require_once "../../database/koneksi.php";
+require_once "../../utils/utils.php";
+$tahun = $_POST['tahun'];
+$id_ruangan = $_POST['id_ruangan'];
+$id_user = $_POST['id_user'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laporan Tamu Tahun <?= $tahun; ?></title>
+    <style>
+        table,
+        th,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        table {
+            width: 100%;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        #kop {
+            display: flex;
+            justify-content: center;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div id="kop" class="d-flex justify-content-center gap-5">
+            <img src="../../assets/img/Lambang_Kota_Banjarbaru.png" height="150" alt="">
+            <div class="text-center" style="flex: 1;">
+                <h2>
+                    DINAS ARSIP DAN PERPUSTAKAAN DAERAH
+                    <br>
+                    BANJARBARU
+                </h2>
+                <p>
+                    Alamat: Jl. Wijaya Kusuma No.7
+                    <br>
+                    Email: darpusdabjb@banjarbarukota.go.id
+                </p>
+            </div>
+        </div>
+        <div class="my-3" style="border-top: 2px solid black; margin-top:12px;"></div>
+        <h2 class="text-center">Laporan Tamu Tahun <?= $tahun; ?></h2>
+        <table>
+            <thead>
+                <tr>
+                    <th class="text-center">No</th>
+                    <th>Nama</th>
+                    <th>Tanggal</th>
+                    <th>Keperluan</th>
+                    <th>Tujuan</th>
+                    <th>Petugas</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $no = 1;
+                $result = $mysqli->query("SELECT * FROM view_buku_tamu WHERE YEAR(tanggal)='$tahun' AND id_ruangan LIKE '%$id_ruangan%' AND id_user LIKE '%$id_user%' ORDER BY id_tamu DESC");
+                ?>
+                <?php if ($result->num_rows) : ?>
+                    <?php while ($row = $result->fetch_assoc()) : ?>
+                        <tr>
+                            <td class="text-center"><?= $no++; ?></td>
+                            <td><?= $row['nama']; ?></td>
+                            <td class="text-center"><?= $row['tanggal']; ?></td>
+                            <td class="text-center"><?= $row['keperluan']; ?></td>
+                            <td class="text-center"><?= $row['nama_ruangan']; ?></td>
+                            <td class="text-center"><?= $row['nama_user']; ?></td>
+                        </tr>
+                    <?php endwhile; ?>
+                <?php endif; ?>
+                <?php $result->free_result(); ?>
+            </tbody>
+        </table>
+    </div>
+    <script>
+        window.print();
+    </script>
+</body>
+
+</html>
--- halaman_laporan/halaman_laporan.php
+++ halaman_laporan/halaman_laporan.php
@@ -97,29 +97,48 @@
                     </div>
                     <!-- END PANEL NO CONTROLS -->
                 </div>
+
                 <div class="col-md-4">
                     <!-- PANEL NO CONTROLS -->
                     <div class="panel">
                         <div class="panel-heading">
-                            <h3 class="panel-title">Laporan Ruangan</h3>
+                            <h3 class="panel-title">Laporan Tamu Per Tahun</h3>
                         </div>
                         <div class="panel-body">
-                            <div class="row" style="margin-bottom: 16px;">
-                                <div class="col-md-12">
-                                    <form action="halaman_laporan/cetak/laporan_ruangan.php" method="POST" target="_blank">
-                                        <label class="form-label">Nama-nama Ruangan</label>
-                                        <button type="submit" name="submit" class="form-control btn btn-primary">Cetak Laporan</button>
-                                    </form>
+                            <form action="halaman_laporan/cetak/laporan_tamu_per_tahun.php" method="POST" target="_blank">
+                                <div class="row" style="margin-bottom: 16px;">
+                                    <div class="col-md-6">
+                                        <label for="">Tahun</label>
+                                        <input type="number" name="tahun" class="form-control" value="<?= Date('Y'); ?>">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label for="id_user" class="form-label">Petugas</label>
+                                        <?php $data_ruangan = $mysqli->query("SELECT * FROM tabel_user WHERE status='PETUGAS'"); ?>
+                                        <select class="form-control" id="id_user" name="id_user">
+                                            <option value="" selected>Semua Petugas</option>
+                                            <?php while ($row = $data_ruangan->fetch_assoc()) : ?>
+                                                <option value="<?= $row['id_user']; ?>"><?= ucwords(strtolower($row['nama'])); ?></option>
+                                            <?php endwhile; ?>
+                                        </select>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <form action="halaman_laporan/cetak/laporan_jumlah_kunjungan_ruangan.php" method="POST" target="_blank">
-                                        <label class="form-label">Jumlah Kunjungan Ruangan</label>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <label for="id_ruangan" class="form-label">Tujuan</label>
+                                        <?php $data_ruangan = $mysqli->query("SELECT * FROM tabel_ruangan ORDER BY nama_ruangan"); ?>
+                                        <select class="form-control" id="id_ruangan" name="id_ruangan">
+                                            <option value="" selected>Semua Ruangan</option>
+                                            <?php while ($row = $data_ruangan->fetch_assoc()) : ?>
+                                                <option value="<?= $row['id_ruangan']; ?>"><?= ucwords(strtolower($row['nama_ruangan'])); ?></option>
+                                            <?php endwhile; ?>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label class="form-label" style="visibility: hidden;">Tombol</label>
                                         <button type="submit" name="submit" class="form-control btn btn-primary">Cetak Laporan</button>
-                                    </form>
+                                    </div>
                                 </div>
-                            </div>
+                            </form>
                         </div>
                     </div>
                     <!-- END PANEL NO CONTROLS -->
@@ -145,6 +164,34 @@
                                 <div class="col-md-12">
                                     <form action="halaman_laporan/cetak/laporan_jumlah_pengunjung_per_pertugas.php" method="POST" target="_blank">
                                         <label class="form-label">Jumlah Pengunjung Tiap Petugas</label>
+                                        <button type="submit" name="submit" class="form-control btn btn-primary">Cetak Laporan</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- END PANEL NO CONTROLS -->
+                </div>
+
+                <div class="col-md-4">
+                    <!-- PANEL NO CONTROLS -->
+                    <div class="panel">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Laporan Ruangan</h3>
+                        </div>
+                        <div class="panel-body">
+                            <div class="row" style="margin-bottom: 16px;">
+                                <div class="col-md-12">
+                                    <form action="halaman_laporan/cetak/laporan_ruangan.php" method="POST" target="_blank">
+                                        <label class="form-label">Nama-nama Ruangan</label>
+                                        <button type="submit" name="submit" class="form-control btn btn-primary">Cetak Laporan</button>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <form action="halaman_laporan/cetak/laporan_jumlah_kunjungan_ruangan.php" method="POST" target="_blank">
+                                        <label class="form-label">Jumlah Kunjungan Ruangan</label>
                                         <button type="submit" name="submit" class="form-control btn btn-primary">Cetak Laporan</button>
                                     </form>
                                 </div>
