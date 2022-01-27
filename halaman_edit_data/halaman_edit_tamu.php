a063ec175c0783a921a8c1960c6d2b8c09e170e8
yafizh <a.chairil.yafizh@gmail.com>
2022-01-27 20:44:18 +0800

    adjusing edit tamu with new table tabel_tamu

--- halaman_edit_data/halaman_edit_tamu.php
+++ halaman_edit_data/halaman_edit_tamu.php
@@ -12,16 +12,22 @@ if (isset($_GET['id_tamu'])) {
         "</script>";
 
 if (isset($_POST['submit'])) {
+    $nomor_identitas = $_POST['nomor_identitas'];
     $nama = $_POST['nama'];
     $alamat = $_POST['alamat'];
     $nomor_handphone = $_POST['nomor_handphone'];
+    $id_ruangan = $_POST['id_ruangan'];
+    $kesan_kunjungan = $_POST['kesan_kunjungan'];
     $keperluan = $_POST['keperluan'];
 
     $sql = "UPDATE tabel_buku_tamu 
             SET 
+                nomor_identitas='$nomor_identitas', 
                 nama='$nama', 
                 alamat='$alamat', 
                 nomor_handphone='$nomor_handphone', 
+                id_ruangan='$id_ruangan', 
+                kesan_kunjungan='$kesan_kunjungan', 
                 keperluan='$keperluan'
             WHERE 
                 id_tamu=" . $_GET['id_tamu'];
@@ -49,14 +55,42 @@ if (isset($_POST['submit'])) {
                     <form action="" method="POST">
                         <div class="row" style="margin-bottom: 16px;">
                             <div class="col-md-12">
+                                <label for="nomor_identitas" class="form-label">Nomor Identitas</label>
+                                <input type="text" class="form-control" id="nomor_identitas" name="nomor_identitas" value="<?= $row['nomor_identitas']; ?>">
+                            </div>
+                        </div>
+                        <div class="row" style="margin-bottom: 16px;">
+                            <div class="col-md-12">
                                 <label for="nama" class="form-label">Nama</label>
                                 <input type="text" class="form-control" id="nama" name="nama" value="<?= $row['nama']; ?>">
                             </div>
                         </div>
                         <div class="row" style="margin-bottom: 16px;">
-                            <div class="col-md-12">
-                                <label for="tanggal" class="form-label">Tanggal Bertamu</label>
-                                <input type="date" class="form-control" readonly id="tanggal" name="tanggal" value="<?= $row['tanggal']; ?>">
+                            <div class="col-md-3">
+                                <label for="hari" class="form-label">Hari</label>
+                                <input type="text" class="form-control" readonly id="hari" name="hari" value="<?= HARI_DALAM_INDONESIA[Date("w", strtotime($row['tanggal']))]; ?>">
+                            </div>
+                            <div class="col-md-3">
+                                <label class="form-label">Tanggal Bertamu</label>
+                                <input type="date" class="form-control" readonly value="<?= $row['tanggal']; ?>">
+                            </div>
+                            <div class="col-md-3">
+                                <label class="form-label">Jam Masuk</label>
+                                <input type="time" class="form-control" readonly value="<?= $row['jam_masuk']; ?>">
+                            </div>
+                            <div class="col-md-3">
+                                <label class="form-label">Jam Keluar</label>
+                                <input type="time" class="form-control" readonly value="<?= $row['jam_keluar']; ?>">
+                            </div>
+                        </div>
+                        <div class="row" style="margin-bottom: 16px;">
+                            <div class="col-md-6">
+                                <label for="nomor_handphone" class="form-label">Nomor Handphone</label>
+                                <input type="number" class="form-control" id="nomor_handphone" name="nomor_handphone" value="<?= $row['nomor_handphone']; ?>">
+                            </div>
+                            <div class="col-md-6">
+                                <label class="form-label">Status Kunjungan</label>
+                                <input type="text" class="form-control" readonly value="<?= ucwords(strtolower($row['status_kunjungan'])); ?>">
                             </div>
                         </div>
                         <div class="row" style="margin-bottom: 16px;">
@@ -66,15 +100,29 @@ if (isset($_POST['submit'])) {
                             </div>
                         </div>
                         <div class="row" style="margin-bottom: 16px;">
-                            <div class="col-md-12">
-                                <label for="nomor_handphone" class="form-label">Nomor Handphone</label>
-                                <input type="number" class="form-control" id="nomor_handphone" name="nomor_handphone" value="<?= $row['nomor_handphone']; ?>">
+                            <div class="col-md-6">
+                                <label for="id_ruangan" class="form-label">Tujuan</label>
+                                <select class="form-control" id="id_ruangan" name="id_ruangan" required>
+                                    <option value="" selected disabled>Pilih Tempat</option>
+                                    <?php $data_ruangan = $mysqli->query("SELECT * FROM tabel_ruangan ORDER BY nama_ruangan"); ?>
+                                    <?php while ($row_ruangan = $data_ruangan->fetch_assoc()) : ?>
+                                        <?php if ($row_ruangan['id_ruangan'] == $row['id_ruangan']) : ?>
+                                            <option selected value="<?= $row_ruangan['id_ruangan']; ?>"><?= ucwords(strtolower($row_ruangan['nama_ruangan'])); ?></option>
+                                        <?php else : ?>
+                                            <option value="<?= $row_ruangan['id_ruangan']; ?>"><?= ucwords(strtolower($row_ruangan['nama_ruangan'])); ?></option>
+                                        <?php endif; ?>
+                                    <?php endwhile; ?>
+                                </select>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="keperluan" class="form-label">Keperluan</label>
+                                <input type="text" class="form-control" id="keperluan" name="keperluan" value="<?= $row['keperluan']; ?>">
                             </div>
                         </div>
                         <div class="row" style="margin-bottom: 16px;">
                             <div class="col-md-12">
-                                <label for="keperluan" class="form-label">Keperluan</label>
-                                <input type="text" class="form-control" id="keperluan" name="keperluan" value="<?= $row['keperluan']; ?>">
+                                <label for="kesan_kunjungan" class="form-label">Kesan Kunjungan</label>
+                                <input type="text" class="form-control" id="kesan_kunjungan" name="kesan_kunjungan" value="<?= $row['kesan_kunjungan']; ?>">
                             </div>
                         </div>
                         <div class="row" style="margin-bottom: 16px;">
